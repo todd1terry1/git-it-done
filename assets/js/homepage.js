ccd167b9b924307f6f8d8ec194171c0d9b6e2a15
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2022-02-04 01:04:25 -0600

    handled hurdles of the 404

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -18,6 +18,11 @@ var username = nameInputEl.value.trim();
 };
 
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
     console.log(repos);
     console.log(searchTerm);
     // clear old content
@@ -65,12 +70,21 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a get request to url
-    fetch(apiUrl).then(function(response) {
-      console.log(response);
+    fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
       response.json().then(function(data) {
         displayRepos(data, user);
       });
-    });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
+  });
   };
 
 
