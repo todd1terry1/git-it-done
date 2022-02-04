4c2c591b70ee6840c3c52f31be18dcec7d52dc2f
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2022-02-03 23:41:17 -0600

    added form El

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,20 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+var username = nameInputEl.value.trim();
+
+     if (username) {
+     getUserRepos(username);
+     nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+    console.log(event);
+};
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -11,5 +28,5 @@ var getUserRepos = function(user) {
     });
   };
   
-  getUserRepos("lernantino");
+  userFormEl.addEventListener("submit", formSubmitHandler);
   
