9bfe842a802a59aba069a47e8938399fb914fb33
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2022-02-06 00:38:31 -0600

    query strings

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,12 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+};
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -73,4 +80,6 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
   };
   
-  getRepoIssues("facebook/react");
+  getRepoName()
+
+ 
